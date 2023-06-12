0890f4111ee44c52eb9c32988737c9b300459dcc
micheleldsk <micheleldsk@gmail.com>
2023-06-11 22:07:14 -0300

    update caches structures

--- src/app/features/user/repository.ts
+++ src/app/features/user/repository.ts
@@ -1,33 +1,49 @@
 import { Repository } from "typeorm";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 } from 'uuid';
 import { DatabaseConnection } from "../../../main/database";
 import { User } from "../../models/user";
 import { UserEntity } from "../../shared/database/entites/user.entity";
 import { UserTipo, UserToCreateDTO } from "./usecases/createUserUsecase";
+import { CacheRepository } from "../../shared/cache/repository";
 
 export class UserRepository {
   private userRepository: Repository<UserEntity>;
+  private cacheRepository: CacheRepository;
 
   constructor() {
     this.userRepository = DatabaseConnection.client.manager.getRepository(UserEntity);
+    this.cacheRepository = new CacheRepository();
   }
 
   async create(userToCreate: UserToCreateDTO): Promise<User> {
     const createdUser = await this.userRepository.save({
-      uuid: uuidv4(),
+      uuid: v4(),
       ...userToCreate
     });
 
+    await invalidateCacheByPrefix('list-users', this.cacheRepository);
     return UserRepository.entityToModel(createdUser);
   }
 
-  async listUsers(filter: Partial<UserEntity>) : Promise<User[]> {
-    const usersFound = await this.userRepository.findBy(filter);
+  async listUsers(filter: Partial<UserEntity>): Promise<User[]> {
+    const usersFound: UserEntity[] = await executeIfNotCached(
+      cacheKeyForObject('list-users', filter),
+      this.cacheRepository,
+      async () => {
+        return this.userRepository.findBy(filter);
+      }
+    )
     return usersFound.map((userEntity) => UserRepository.entityToModel(userEntity));
   }
 
-  async find(uuid: string) : Promise<User | null> {
-    const userFound = await this.userRepository.findOneBy({ uuid });
+  async find(uuid: string): Promise<User | null> {
+    const userFound =  await executeIfNotCached(
+      `find-user-${uuid}`,
+      this.cacheRepository,
+      async () => {
+        return this.userRepository.findOneBy({ uuid });
+      }
+    )
     return userFound && UserRepository.entityToModel(userFound);
   }
 
